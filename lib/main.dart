8736ac3e28f540a4ae8ad08c12ae7625ecad6d59
Amm Dhillon <ammdhillon@gmail.com>
2023-02-03 11:15:17 +0530

    Day two

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:demo/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,18 +6,12 @@ void main() {
 }
 
 class MyFirstApp extends StatelessWidget {
-  const MyFirstApp({super.key});
+  // const MyFirstApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome My First app"),
-          ),
-        ),
-      ),
+      home:HomePage()
     );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,19 +1,24 @@
 import 'package:demo/main.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/widgets.dart';
 
-void main() {
-  runApp(MyWidget());
-}
-
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
-
+class HomePage extends StatelessWidget {
+  double days = 30;
+  String welcome = "Welcome My First app Flutter app";
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Container(
-        child: Text("Welcome My first app"),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My App"),
       ),
+      body: Center(
+        child: Container(
+          child: Text(welcome),
+        ),
+      ),
+      drawer: Drawer(),
     );
   }
 }
